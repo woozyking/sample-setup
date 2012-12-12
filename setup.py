5389d1b702f4ed4e22b801c0acf9b705365d8a0a
Runzhou Li (Leo) <runzhou@addictivemobility.com>
2012-12-12 15:55:49 -0500

    Sub package

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ from distutils.core import setup
 
 setup(name='sample_lib',
     version='1.0',
-    py_modules=['sample_lib.m1', 'sample_lib.m2']  # relative path to modules with . as separator
+    py_modules=['sample_lib.m1',
+                'sample_lib.m2',
+                'sample_lib.sub.m3']  # relative path to modules with . as separator
     )
