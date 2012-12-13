5f320cf8185c6314f414cb6db79e7e6950144e9d
Runzhou Li (Leo) <runzhou@addictivemobility.com>
2012-12-13 09:58:06 -0500

    Intra-package import practices

--- sample_lib/sub/m3.py
+++ sample_lib/sub/m3.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# For compatibility and portability
+from __future__ import absolute_import, division, with_statement
+from sample_lib import m1
+
 
 def atimesb(a, b):
     return a * b
+
+
+def m1func(a, b):
+    return m1.aplusb(a, b)
