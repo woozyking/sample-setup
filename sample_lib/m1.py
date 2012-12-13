cb623cf19527f253c5b7ceea360b44712d03ec2a
Runzhou Li (Leo) <runzhou@addictivemobility.com>
2012-12-13 13:11:57 -0500

    Install by package, another alternative

--- sample_lib/m1.py
+++ sample_lib/m1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import, division, with_statement
 
 def aplusb(a, b):
     return a + b
--- setup.py
+++ setup.py
@@ -3,9 +3,18 @@
 
 from distutils.core import setup
 
-setup(name='sample_lib',
+setup(
+    name='sample_lib',
+    description='Sample usage of Python distutils.core.setup module',
+    author='nobody',
+    author_email='nobody@example.com',
+    url='https://nobody.example.com',
     version='1.0',
-    py_modules=['sample_lib.m1',
-                'sample_lib.m2',
-                'sample_lib.sub.m3']  # relative path to modules with . as separator
-    )
+    # Lazy way
+    package_dir={'sample_lib': 'sample_lib'}, # package: dir
+    packages=['sample_lib', 'sample_lib.sub']
+    # Hand picking way
+    # py_modules=['sample_lib.m1',
+    #             # 'sample_lib.m2', # excluded from building
+    #             'sample_lib.sub.m3']  # relative path to modules with . as separator
+)
